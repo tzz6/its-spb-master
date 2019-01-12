f3c47a5fd31fffed4b0608d55b2fbbc3d9264a6a
tzz6 <291232797@qq.com>
2019-01-12 15:26:54 +0800

    its-spb

---
+++ its-spb-geteway/src/main/java/com/its/gateway/ItsSpbGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.its.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ItsSpbGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsSpbGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RequestTimeGatewayFilterFactory requestTimeGatewayFilterFactory() {
+		return new RequestTimeGatewayFilterFactory();
+	}
+}
